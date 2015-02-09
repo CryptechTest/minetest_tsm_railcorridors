ddb5da4eb0908646d5efd36ab64ac94f81568f7d
Wuzzy <almikes@aol.com>
2015-02-10 00:54:12 +0100

    Fix variable leaking into global space

--- init.lua
+++ init.lua
@@ -255,7 +255,7 @@ function corridor_func(waypoint, coord, sign, up_or_down, up)
 	end
 	if not up_or_down then
 		for i=1,segcount do
-			p = {x=waypoint.x+vek.x*i, y=waypoint.y+vek.y*i-1, z=waypoint.z+vek.z*i}
+			local p = {x=waypoint.x+vek.x*i, y=waypoint.y+vek.y*i-1, z=waypoint.z+vek.z*i}
 			if minetest.get_node({x=p.x,y=p.y-1,z=p.z}).name=="air" and minetest.get_node({x=p.x,y=p.y-3,z=p.z}).name~="default:rail" then
 				p.y = p.y - 1;
 			end
